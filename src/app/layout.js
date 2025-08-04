b14f6a745d3df35fa5a4a92ffd7cd2dd5fc8826d
osama <om2951855@gmail.com>
2025-08-04 17:42:20 +0300

    fix layout design

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,12 +4,13 @@ import { AuthProvider } from "@/components/AuthProvider";
 
 export const metadata = {
     title: "Mini CRM",
+    description: "A simple CRM application",
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className="bg-gray-100">
+            <body className="bg-[url('/bg-main.svg')] bg-cover">
                 <ToastContainer position="top-center" autoClose={1000} />
                 <AuthProvider>{children}</AuthProvider>
             </body>
